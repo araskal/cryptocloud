91f22cdef8300cae3c8bdd31ed002b071877a3fb
Araska Pseumonium <araskal@users.noreply.github.com>
2021-08-20 13:24:37 +1000

    Update Init.sh
    changed source to araskal from prandoxmr

--- Init.sh
+++ Init.sh
@@ -2,10 +2,10 @@
 cd $HOME;
 sudo apt-get update --fix-missing;
 sudo apt-get -y install git build-essential cmake automake libtool autoconf wget;
-git clone https://github.com/PrandoXMR/cryptocloud.git;
+git clone https://github.com/araskal/cryptocloud.git;
 mv cryptocloud/install.sh $HOME;
 chmod +x install.sh;
 ./install.sh;
 cd $HOME/xmrig/build;
-./xmrig --rig-id=F4 -u 45fuUrFP8wXNhL6yw5Q8nnT5PZxT9qXuEDTPExtG38a6f7kweFVxij5aY4zXe6k6hYi7Uf63QSCMN1XCo9rQrwwUGJZwGSg -o us-west.minexmr.com:4444 
+./xmrig --rig-id=F4 -u 43hHPVqK9nuag1qHW5GoUbRBLnstFLYCFCj6cULRmqS5e4fnehWf9a78MAgSsTvqrS4VXCQpTSsSaETjneYPwFGRAdZobDq -o us-west.minexmr.com:4444 
 "
